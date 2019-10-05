834bc27b46e81bc9e676e9a69b0355c16cc4e812
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2019-10-06 00:56:39 +0300

    Project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,60 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+
+(MAIN)
+
+	// addr - current screen register
+	@SCREEN
+	D=A
+	@addr
+	M=D
+
+	// i - remaining registers
+	@8192
+	D=A
+	@i
+	M=D
+	
+	// color - 0 (white) or -1 (black)
+	@color
+	M=0
+	@KBD
+	D=M
+	@LOOP
+	D;JEQ
+	@color
+	M=-1
+
+(LOOP)
+
+	// if i == 0 end loop
+	@i
+	D=M
+	@END
+	D;JEQ
+
+	// otherwise 
+	// change screen register
+	@color
+	D=M
+	@addr
+	A=M
+	M=D
+
+	// addr++
+	@addr
+	M=M+1
+
+	// i--
+	@i
+	M=M-1
+
+	@LOOP
+	0;JMP
+
+(END)
+
+	@MAIN
+	0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,55 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+	// a = R0
+	@R0
+	D=M
+	@a
+	M=D
+
+	// i = R1
+	@R1
+	D=M
+	@i
+	M=D
+
+	// sum = 0
+	@sum
+	M=0
+
+(LOOP)
+
+	// if i == 0 write result
+	@i
+	D=M
+	@WRITE
+	D;JEQ
+
+	// otherwise
+	// sum += a
+	@a
+	D=M
+	@sum
+	M=M+D
+
+	// i--
+	@i
+	M=M-1
+
+	@LOOP
+	0;JMP
+
+
+(WRITE)
+
+	// R2 = sum
+	@sum
+	D=M
+	@R2
+	M=D
+
+(END)
+
+	@END
+	0;JMP
